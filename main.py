94f1297bb28d50045cece9f705d77fc4f7c5b15e
Cassiano Junior <cassiano.junior@hotmail.com>
2025-05-27 18:49:20 -0300

    create post route, create test, create products list json db

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from json_db import JsonDB
+from product import Product
 from indb import generate_products
 
 app = FastAPI()
 
-products = generate_products()
+productDB = JsonDB(path='./data/products.json')
 
-@app.get('/')
+@app.get('/products')
 def get_products():
+    products = productDB.read()
     return { "products" : products}
+
+
+@app.post('/products')
+def create_product(product: Product):
+    
+    
+    productDB.insert(product)
+
+    return { "status" : "inserted"}
